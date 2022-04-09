85f244ae2c9157d16b4201f95f7fd57cd4d95229
davletshinilnar <93643164+davletshinilnar@users.noreply.github.com>
2022-04-09 23:35:02 +0300

    Update PHP

--- src/mailer/smart.php
+++ src/mailer/smart.php
@@ -18,7 +18,7 @@ $mail->Password = 'cvquqrjfxgtpcpul';                           // Наш пар
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to
  
-$mail->setFrom('k1mass.id@gmail.com', 'Pulse');   // От кого письмо 
+$mail->setFrom('urhrhbe@gmail.cum', 'Pulse');   // От кого письмо 
 $mail->addAddress('heloy33388@nahetech.com');     // Add a recipient
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
